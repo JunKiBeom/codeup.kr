d9305cbf6c6d69d2a543e3916231abce0fa9a710
JunKiBeom <dona0408@gmail.com>
2019-07-02 00:14:10 +0900

    up

---
+++ 1527_7858322(AC).c
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+
+void f(){
+        printf("123");
+    }
+int main()
+{
+    f();
+    return 0;
+}
---
+++ 1709_7807342(AC).py
@@ -0,0 +1,7 @@
+﻿# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+num=int(input())
+arr=[int(i) for i in input().split()]
+arr.sort(reverse=True)
+for i in arr:
+    print(i,end=" ")
