8df5dcd13ab65d3ae1baaa93c44e11a51a1f4d11
JunKiBeom <dona0408@gmail.com>
2021-02-19 02:30:43 +0900

    Warm Up

---
+++ 1282_17074497(AC).java
@@ -0,0 +1,17 @@
+﻿import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result;
+        for (int i=0;i<=n;i++) {
+            result = n - i;
+            if (result % Math.sqrt(result) == 0) {
+                System.out.println(i + " " + (int) Math.sqrt(result));
+                break;
+            }
+        }
+    }
+}
+
---
+++ 1285_17076147(AC).py
@@ -0,0 +1,32 @@
+﻿import re
+inp = input()
+l = re.split("\W+",inp)
+op = re.split("\w+",inp)
+l.remove('')
+op.remove('')
+
+index=1
+for i in op:
+    l.insert(index, i)
+    index+=2
+
+# print(l)
+
+l.reverse()
+result=int(l.pop())
+
+for i in range(len(op)):
+    temp=l.pop()
+    result=int(result)
+    if temp=='+':
+        result+=int(l.pop())
+    elif temp=='-':
+        result-=int(l.pop())
+    elif temp=='*':
+        result*=int(l.pop())
+    elif temp=='/':
+        result/=int(l.pop())
+    elif temp=='=':
+        break
+
+print(int(result))
