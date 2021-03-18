7773b123cdcad005ab3d84b5004db76ef5b126a3
JunKiBeom <dona0408@gmail.com>
2021-03-19 01:58:25 +0900

    warm-up

---
+++ 1540_17592987(AC).java
@@ -0,0 +1,18 @@
+﻿import java.util.Scanner;
+
+public class Main {
+    public void check(int a) {
+        if (a==0)
+            System.out.println("zero");
+        else
+            System.out.println("non zero");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        Main m = new Main();
+        m.check(a);
+//        System.out.println(check(a));
+    }
+}
---
+++ 1541_17592988(AC).java
@@ -0,0 +1,20 @@
+﻿import java.util.Scanner;
+
+public class Main {
+    public void check(int a) {
+        if (a==0)
+            System.out.println("zero");
+        else if (a>0)
+            System.out.println("positive");
+        else
+            System.out.println("negative");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        Main m = new Main();
+        m.check(a);
+//        System.out.println(check(a));
+    }
+}
