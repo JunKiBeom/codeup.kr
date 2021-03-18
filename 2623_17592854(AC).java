1bdbaf5cbfa010ee0e5f89d2ea894cfcf706e858
JunKiBeom <dona0408@gmail.com>
2021-03-19 01:31:35 +0900

    warm-up

---
+++ 2623_17592854(AC).java
@@ -0,0 +1,21 @@
+﻿import java.util.Scanner;
+
+public class Main {
+    static int GCD(int a, int b) {
+        while (a*b!=0){
+            if (a>b)
+                a%=b;
+            else
+                b%=a;
+        }
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(GCD(a,b));
+    }
+}
