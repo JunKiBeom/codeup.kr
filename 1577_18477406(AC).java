9bfe28b572bc8e5c44ea180cbccf5da90669c7a3
JunKiBeom <dona0408@gmail.com>
2021-04-18 21:30:38 +0900

    warm up

---
+++ 1577_18477406(AC).java
@@ -0,0 +1,15 @@
+﻿import java.util.Scanner;
+
+public class Main {
+    public static int myabs(int n) {
+        if (n<0)
+            return n*-1;
+        return n;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(myabs(n));
+    }
+}
