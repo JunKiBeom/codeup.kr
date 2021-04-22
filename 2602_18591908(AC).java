e0c68703237179211f2c1c2fcf10ef852f4dbebd
전기범 <37178265+JunKiBeom@users.noreply.github.com>
2021-04-22 23:47:10 +0900

    warm up

---
+++ 2602_18591908(AC).java
@@ -0,0 +1,24 @@
+﻿import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] score = new int[4][4];
+        for (int i=0;i<3;i++)
+            for (int j=0;j<3;j++)
+                score[i][j] = sc.nextInt();
+
+        for (int i=0;i<3;i++) {
+            for (int j = 0; j < 3; j++) {
+                score[i][3] += score[i][j];
+                score[3][i] += score[j][i];
+            }
+            score[3][3] += score[i][3];
+        }
+        for (int i=0;i<4;i++) {
+            for (int j = 0; j < 4; j++)
+                System.out.print(score[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
---
+++ 2701_18592078(AC).java
@@ -0,0 +1,21 @@
+﻿import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[5];
+        int min=Integer.MAX_VALUE;
+
+        for (int i=0;i<5;i++)
+            arr[i] = sc.nextInt();
+
+        int tmp;
+        for (int i=0;i<5;i++) {
+            tmp=Math.abs(n-arr[i]);
+            if (min>=tmp)
+                min=tmp;
+        }
+        System.out.println(min);
+    }
+}
