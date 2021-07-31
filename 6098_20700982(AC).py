e584d147cd6a5023a6f80357090083db9cbfb9b3
전기범 <dona0408@naver.com>
2021-07-31 19:52:58 +0900

    warm up

---
+++ 6098_20700982(AC).py
@@ -0,0 +1,26 @@
+﻿Map = [[] * 10 for _ in range(10)]
+for i in range(10):
+    Map[i] = list(map(int, input().split()))
+x, y = 1, 1
+
+while (True):
+    if (Map[x][y] == 2):
+        Map[x][y] = 9
+        break
+    if (Map[x][y + 1] != 1):
+        Map[x][y] = 9
+        y += 1
+    else:
+        if (Map[x + 1][y] != 1):
+            Map[x][y] = 9
+            x += 1
+        else:
+            Map[x][y]=9
+            break
+
+# print("\\\\\\\\\\\\\\\\")
+for i in range(10):
+    for j in range(10):
+        print(Map[i][j], end=' ')
+    print()
+
