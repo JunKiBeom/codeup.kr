37e5fe2697f2b5a14693d77797489ea2f4118f42
전기범 <dona0408@naver.com>
2021-08-12 01:01:35 +0900

    warm up

---
+++ 1615_20804046(AC).java
@@ -0,0 +1,35 @@
+﻿import java.util.Scanner;
+
+public class Main {
+    static int SelfN(int n) {
+        for (int i=0;i<=n;i++) {
+            int tmpNum = i;
+            int tmpVal = 0;
+
+            while (tmpNum > 0) {    // 각 자리수 더하는 과정
+                tmpVal+=tmpNum%10;
+                tmpNum/=10;
+            }
+
+            if (n==tmpVal+i) {    // 제너레이터 찾음
+                return 0;
+            }
+        }
+        return n;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int num = 0;
+
+        for (int i = a; i <= b; i++) {
+            num+=SelfN(i);
+        }
+
+        System.out.println(num);
+    }
+
+}
+
---
+++ 6079_20803063(AC).py
@@ -0,0 +1,7 @@
+﻿n = int(input())
+num=0
+i=0
+while(num<n):
+    i+=1
+    num+=i
+print(i)
